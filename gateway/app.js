8c7af2eca2098cdb78577eee60c149c1b425ad9f
pavelbugaian <pbugaian@fusionworks.md>
2022-01-20 22:26:29 +0200

    added redis caching middleware

--- gateway/app.js
+++ gateway/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const flatCache = require('flat-cache');
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const redis = require('redis');
+const axios = require('axios')
 
 const PORT = 80;
 const HOST = 'localhost';
@@ -9,12 +11,12 @@ const children = {
   'fileService': [
     'http://dap-course_file_service1_1:3050',
     'http://dap-course_file_service2_1:3051',
-    'http://dap-course_file_service3_1:3052'
+    'http://dap-course_file_service3_1:3052',
   ],
   'userService': [
     'http://dap-course_user_service_1:8084',
     'http://dap-course_user_service_1:8084',
-    'http://dap-course_user_service_1:8084'
+    'http://dap-course_user_service_1:8084',
   ],
 };
 
@@ -30,25 +32,13 @@ const currentChildIterator = (childrenSize, start = -1) => {
   };
 };
 
-const cache = flatCache.load('cache');
-const flatCacheMiddleware = (req, res, next) => {
-  const key = '__express__' + req.originalUrl || req.url;
-  const cacheContent = cache.getKey(key);
+const redisClient = redis.createClient({ url: 'redis://dap-course_redis_1:6379' });
 
-  if (cacheContent) {
-    res.send(cacheContent);
-  } else {
-    res.sendResponse = res.send;
-    res.send = (body) => {
-      cache.setKey(key, body);
-      cache.save();
+(async () => {
+  redisClient.on('error', (err) => console.log('Redis Client Error', err));
 
-      res.sendResponse(body);
-    };
-
-    next();
-  }
-};
+  await redisClient.connect();
+})();
 
 const app = express();
 
@@ -56,7 +46,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', flatCacheMiddleware);
+// app.get('/', flatCacheMiddleware);
 
 // Authorization
 app.use('/', (req, res, next) => {
@@ -66,6 +56,35 @@ app.use('/', (req, res, next) => {
     res.sendStatus(403);
   }
 });
+
+// create redis middleware
+let redisMiddleware = async (req, res, next) => {
+  if (req.method !== 'GET') {
+    // next();
+    return;
+  }
+
+  const url = req.url || req.originalUrl;
+  console.log(`url ${url}`);
+
+  const redisData = await redisClient.get(url);
+  console.log(`redisData ${redisData}`);
+
+  if (redisData !== null) {
+    console.log('got from redis');
+
+    return res.send(JSON.parse(redisData));
+  } else {
+    axios.get(url, { headers: req.headers }).then(r => {
+      console.log(r.statusText);
+      redisClient.setEx(req.url || req.originalUrl, 3600, JSON.stringify(r.data));
+      console.log('got from server');
+
+      return res.send(r.data);
+    });
+  }
+
+};
 
 app.use(
   '/fileService',
@@ -78,11 +97,10 @@ app.use(
       const serviceChildren = children['fileService'];
       const childrenSize = serviceChildren.length;
       const getCurrentChild = currentChildIterator(childrenSize);
-      const url = serviceChildren[getCurrentChild()];
 
-      return url
+      return serviceChildren[getCurrentChild()];
     },
-  })
+  }),
 );
 
 app.use(
@@ -96,12 +114,13 @@ app.use(
       const serviceChildren = children['userService'];
       const childrenSize = serviceChildren.length;
       const getCurrentChild = currentChildIterator(childrenSize);
-      const url = serviceChildren[getCurrentChild()];
 
-      return url
+      return serviceChildren[getCurrentChild()];
     },
   }),
 );
+
+app.use(redisMiddleware);
 
 app.listen(PORT, () => {
   console.log(`Starting Proxy at ${HOST}:${PORT}`);
